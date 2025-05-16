e159a968e5cce61c8a31b7ee09b4379cbb2474d5
jasim8799 <mdjasimm107@gmail.com>
2025-05-16 19:05:56 +0530

    update error handler

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
   try {
     const movie = new Movie(req.body);
     await movie.save();
-    res.status(201).json(movie);
+    res.status(201).json({ movie: movie });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }

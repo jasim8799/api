1d75ce7f85fae326dee9e7c77b1481a31ed926b3
jasim8799 <mdjasimm107@gmail.com>
2025-05-16 12:48:09 +0530

    new api updated to fetch and accept url

--- routes/movies.js
+++ routes/movies.js
@@ -29,14 +29,4 @@ router.post('/', async (req, res) => {
   }
 });
 
-// GET: Fetch all movies
-router.get('/', async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.json(movies);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
 module.exports = router;

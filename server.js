1c626b9c6b63421ab81c0b9568b5272664d4e506
jasim8799 <mdjasimm107@gmail.com>
2025-07-24 20:08:53 +0530

    Switched to proxyEventLogger route

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/api/app', require('./routes/appVersion.routes'));
 app.use('/api/analytics', require('./routes/analytics'));
 app.use('/api/appstats', require('./routes/appStats.routes'));
 app.use('/api/crashes', require('./routes/crash.routes'));
-app.use('/api/proxy-analytics', require('./routes/proxyAnalytics'));
+//app.use('/api/proxy-analytics', require('./routes/proxyAnalytics'));
 app.use('/api/proxy-events', require('./routes/proxyEventLogger'));
 
 
